91096ba1773145779c0b604256cc11f46b6f1ff2
LCGG-Group <LCGG-Group@users.noreply.github.com>
2018-10-04 09:38:58 +0800

    Added Fragments

--- app/src/main/java/com/lcgg/price2beat/MainActivity.java
+++ app/src/main/java/com/lcgg/price2beat/MainActivity.java
@@ -7,6 +7,9 @@ import com.microsoft.appcenter.crashes.Crashes;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -20,18 +23,28 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText("Home");
+                    //mTextMessage.setText("Home");
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_store:
-                    mTextMessage.setText("Store");
+                    //mTextMessage.setText("Store");
+                    fragment = new StoreFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText("Notifications");
+                    //mTextMessage.setText("Notifications");
+                    fragment = new NotificationFragment();
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_settings:
-                    mTextMessage.setText("Settings");
+                    //mTextMessage.setText("Settings");
+                    fragment = new SettingsFragment();
+                    loadFragment(fragment);
                     return true;
             }
             return false;
@@ -46,9 +59,19 @@ public class MainActivity extends AppCompatActivity {
         AppCenter.start(getApplication(), "59166ca9-1113-49ca-b46d-65c940fab401",
                 Analytics.class, Crashes.class);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        loadFragment(new HomeFragment());
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
