0d6a8e038c81efd2e9a490974496db3663668d35
LCGG-Group <LCGG-Group@users.noreply.github.com>
2018-10-23 14:22:02 +0800

    - Change Profile

--- app/src/main/java/com/lcgg/price2beat/SettingsFragmentProfile.java
+++ app/src/main/java/com/lcgg/price2beat/SettingsFragmentProfile.java
@@ -9,8 +9,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,6 +42,11 @@ public class SettingsFragmentProfile extends Fragment {
     DatabaseReference refUser;
 
     TextView editName, editEmail, editPoints;
+    EditText updateFirst, updateMiddle, updateLast;
+
+    LinearLayout displayLinear, updateLinear;
+
+    Button btnUpdate, btnEdit;
     User user;
 
     public SettingsFragmentProfile() {
@@ -59,9 +66,21 @@ public class SettingsFragmentProfile extends Fragment {
 
         final View view = inflater.inflate(R.layout.fragment_settings_profile, container, false);
 
+        //updateLinear = (LinearLayout) view.findViewById(R.id.updateLinear);
+        //displayLinear = (LinearLayout) view.findViewById(R.id.displayLinear);
+
+        //updateLinear.setVisibility(View.GONE);
+        //displayLinear.setVisibility(View.VISIBLE);
+
         editName = (TextView) view.findViewById(R.id.profileName);
         editEmail = (TextView) view.findViewById(R.id.profileEmail);
         editPoints = (TextView) view.findViewById(R.id.points);
+
+        updateFirst = (EditText) view.findViewById(R.id.editProfileFirst);
+        updateMiddle = (EditText) view.findViewById(R.id.editProfileMiddle);
+        updateLast = (EditText) view.findViewById(R.id.editProfileLast);
+
+        btnUpdate = (Button) view.findViewById(R.id.updateProfile);
 
         FirebaseUser firebaseUser = auth.getCurrentUser();
         database = FirebaseDatabase.getInstance();
@@ -70,9 +89,57 @@ public class SettingsFragmentProfile extends Fragment {
         refUser = database.getReference("User").child(auth.getUid());
         refUser.addListenerForSingleValueEvent(valueEventListener);
 
+        btnUpdate.setOnClickListener(updateListener);
+        //btnEdit.setOnClickListener(updateListenerEdit);
+
         return view;
     }
 
+    private View.OnClickListener updateListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            refUser.addListenerForSingleValueEvent(valueEventListenerUpdate);
+        }
+    };
+
+    private View.OnClickListener updateListenerEdit = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //updateLinear.setVisibility(View.VISIBLE);
+            //displayLinear.setVisibility(View.GONE);
+        }
+    };
+
+    private ValueEventListener valueEventListenerUpdate = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            user = dataSnapshot.getValue(User.class);
+
+            String firstName = updateFirst.getText().toString();
+            String middleName = updateMiddle.getText().toString();
+            String lastName = updateLast.getText().toString();
+
+            String displayName = firstName + " " + lastName;
+
+            //Set
+            refUser.child("displayName").setValue(displayName);
+            refUser.child("firstName").setValue(firstName);
+            refUser.child("middleName").setValue(middleName);
+            refUser.child("lastName").setValue(lastName);
+
+            //Display
+            editName.setText(user.getDisplayName());
+            editEmail.setText(user.getEmail());
+            editPoints.setText(user.getPoints().toString());
+
+            //updateLinear.setVisibility(View.GONE);
+            //displayLinear.setVisibility(View.VISIBLE);
+        }
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
     private ValueEventListener valueEventListener = new ValueEventListener() {
         @Override
         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
