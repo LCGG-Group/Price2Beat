39e61070b81ca3a167522814b3552d5372315e2b
LCGG-Group <LCGG-Group@users.noreply.github.com>
2018-10-29 10:53:40 +0800

    - Fix AppBar

--- app/src/main/java/com/lcgg/price2beat/MainActivity.java
+++ app/src/main/java/com/lcgg/price2beat/MainActivity.java
@@ -119,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
         loadFragment(new HomeFragment());
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
         setSupportActionBar(myToolbar);
 
         getSupportActionBar().setTitle("Home");
