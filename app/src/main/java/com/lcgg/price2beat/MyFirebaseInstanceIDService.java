9bd6003ea670bc1cdf3bfd4d30b7fafc82177658
len.carlo.g.gonzales <LCGG_Group@outlook.com>
2019-03-14 16:14:03 +0800

    - Updated Firebase Service

--- app/src/main/java/com/lcgg/price2beat/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/lcgg/price2beat/MyFirebaseInstanceIDService.java
@@ -1,19 +1,61 @@
 package com.lcgg.price2beat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
-    private static final String TAG = "FirebaseIIDServiceDemo";
-    public String[] name;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
+
+    @Override    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+
+    }
+
+    private void showNotification(String title, String body) {
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.lcgg.price2beat.test";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("LCGG Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[] {0,1000,500,1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+            notificationBuilder.setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setContentInfo("Info");
+
+            notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+        }
+    }
 
     @Override
-    public void onTokenRefresh() {
-        String token = FirebaseInstanceId.getInstance().getToken();
-        System.out.println("my firebase token " + token );
-    }
-    private void sendRegistrationToServer(String token) {
+    public void onNewToken(String s) {
+        super.onNewToken(s);
 
+        Log.d("TOKENFIREBASE", s);
     }
-}  
+}
