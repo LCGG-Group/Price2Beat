73fc60ca6400a68a91710c599e0bef48b72d518f
len.carlo.g.gonzales <LCGG_Group@outlook.com>
2019-03-19 16:09:59 +0800

    - Deleted notification

--- app/src/main/java/com/lcgg/price2beat/NotificationFragment.java
+++ app/src/main/java/com/lcgg/price2beat/NotificationFragment.java
@@ -1,16 +1,10 @@
 package com.lcgg.price2beat;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
 
 
 public class NotificationFragment extends Fragment {
@@ -30,20 +24,10 @@ public class NotificationFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_notification, container, false);
 
+
         return view;
     }
 
-    public class MyFirebaseMessagingService extends FirebaseMessagingService {
-        private static final String TAG = "FCM Service";
-        @Override
-        public void onMessageReceived(RemoteMessage remoteMessage) {
-            // TODO: Handle FCM messages here.
-            // If the application is in the foreground handle both data and notification messages here.
-            // Also if you intend on generating your own notifications as a result of a received FCM
-            // message, here is where that should be initiated.
-            Log.d(TAG, "From: " + remoteMessage.getFrom());
-            Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-        }
-    }
-
 }
+
+
