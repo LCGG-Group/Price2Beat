7efc4ec53d1602202ad3613c2b30158b886aa18c
len.carlo.g.gonzales <LCGG_Group@outlook.com>
2019-04-03 15:17:32 +0800

    - Bug Fixes - Updated PayPal

--- app/src/main/java/com/lcgg/price2beat/Config/Config.java
+++ app/src/main/java/com/lcgg/price2beat/Config/Config.java
@@ -1,6 +1,6 @@
 package com.lcgg.price2beat.Config;
 
 public class Config {
-    public static final String PAYMENT_CLIENT_ID_SANDBOX = "Aa8iNnCm3y8of2VDhFbiFc3jwUiH_SgC6bhvfkEUWhvnMSRmGjLMPPYtk4-Gd4a3SolOMSHF1g0vLcKL";
-    public static final String PAYMENT_CLIENT_ID_LIVE= "AazG0telPoLgHOOx35no77MtrzT2pDL4akW4P_rAZkCe5_QgANwtm4DcQJlskqVrx9u5I30_smxPgA3Z";
+    public static final String PAYMENT_CLIENT_ID_SANDBOX = "AYLZSMt5ok-DNQ3RgZSlbcgINiNTEVa9OHyCOtucsTQPM6FXJF8rCZnU4Vyo5_hjKRUpuVIFk6c7366j";
+    public static final String PAYMENT_CLIENT_ID_LIVE= "AXZ9TIAOw5ot4jPwjuUBM86zcnLwUINkjmVCXTRhJxQ8KJafuAT9_TprUTXK8pBajZ2_amu6TaezFKtw";
 }
